abf27ae01e274e176635012e9971195db7f9f3e0
Stefan Pratter <stefan@20c.com>
2016-08-20 06:44:10 +0000

    standalone test for import loader

--- tests/test_import_loader.py
+++ tests/test_import_loader.py
@@ -3,6 +3,7 @@ import logging
 import os
 import pytest
 import sys
+import importlib
 
 
 # log here to see import logs
@@ -26,4 +27,11 @@ def test_static_import():
 
 def test_dyn_import():
     from pluginmgr_test.plugins import mod0
+
+
+def test_standalone_import():
+    hook = pluginmgr.SearchPathImporter("standalone", os.path.join(os.path.dirname(__file__), "data", "standalone"), False)
+    sys.meta_path.append(hook)
+    mod = importlib.import_module("standalone.mod0.submodule")
+    assert mod.test == 1
 
