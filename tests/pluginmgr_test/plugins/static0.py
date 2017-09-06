4a076c3fd08affa28d9ff16f585137f9c5c326cf
Matt Griswold <grizz@20c.com>
2017-09-06 21:25:44 +0000

    fix tests for ctor args/kwargs

--- tests/pluginmgr_test/plugins/static0.py
+++ tests/pluginmgr_test/plugins/static0.py
@@ -1,6 +1,13 @@
 
-from pluginmgr.config import PluginBase
+import pluginmgr.config
 from pluginmgr_test import plugin
+
+
+class PluginBase(pluginmgr.config.PluginBase):
+    def __init__(self, config, *args, **kwargs):
+        super(PluginBase, self).__init__(config)
+        self.args = args
+        self.kwargs = kwargs
 
 
 @plugin.register('static0')
