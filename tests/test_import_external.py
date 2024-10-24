617813d8a526cc9578a9295b0ee7b035ab163c49
Samsul <samsulhadikediri@gmail.com>
2024-10-24 19:09:08 +0700

    Add unit test

---
+++ tests/test_import_external.py
@@ -0,0 +1,15 @@
+import logging
+
+import pluginmgr_test.plugins
+import pytest
+
+# log here to see import logs
+logging.basicConfig(level=logging.DEBUG)
+
+import pluginmgr_test
+
+def test_import_external_no_keyerror():
+    try:
+        pluginmgr_test.plugin.import_external()
+    except KeyError:
+        pytest.fail("KeyError was raised when it should not have been.")
